271d99499f5f9499216e465c4524c86805665573
ITITIU20226 <91868746+TaKhang@users.noreply.github.com>
2021-11-22 16:58:14 +0700

    Update Exercise3.c

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -16,6 +16,39 @@ Ex:
 void Ex3(char *str){
 	//Your codes here
 	
+	char word[20]; 
+        char l[20], s[20], c;
+        int i=0, j=0, tie=0;
+		int isspace(), isalnum();
+        str[i - 1] = '\0';
+        for (i = 0; i < strlen(str); i++)
+        {
+            while (i < strlen(str) && !isspace(str[i]) && isalnum(str[i]))
+            {
+                word[j++] = str[i++];
+            }
+            if (j != 0)
+            {
+                word[j] = '\0';
+                if (!tie)
+                {
+                    tie = !tie;
+                    strcpy(l, word);
+                    strcpy(s, word);
+                }
+                if (strlen(word) > strlen(l))
+                {
+                    strcpy(l, word);
+                }
+                if (strlen(word) < strlen(s))
+                {
+                    strcpy(s, word);
+                }
+                j = 0;
+            }
+        }
+        printf("Shortest word: %s\n", s);
+        printf("Longest word: %s",l);
 }
 
 int main(int argc, char *argv[]) {
